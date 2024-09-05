42846f2851fbc10414323e3b8dedc460e59c769b
chajus1 <chajus1@googlemail.com>
2024-09-05 06:01:44 +0100

    building layout

--- Clock/MainPage.xaml.cs
+++ Clock/MainPage.xaml.cs
@@ -9,16 +9,36 @@
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        //private void OnCounterClicked(object sender, EventArgs e)
+        //{
+        //    count++;
+
+        //    if (count == 1)
+        //        CounterBtn.Text = $"Clicked {count} time";
+        //    else
+        //        CounterBtn.Text = $"Clicked {count} times";
+
+        //    SemanticScreenReader.Announce(CounterBtn.Text);
+        //}
+       
+        private void Controls_Clicked(object sender, EventArgs e)
         {
-            count++;
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+        }
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+        private void ColorBtn_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Menu_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Clock_Clicked(object sender, EventArgs e)
+        {
+
         }
     }
 
